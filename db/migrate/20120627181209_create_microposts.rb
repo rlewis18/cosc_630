5828fcd5109c0bab6575064b03f76fcd8a5862bd
Richard Lewis <rlewis18@comcast.net>
2012-06-27 14:14:23 -0400

    database creation

---
+++ db/migrate/20120627181209_create_microposts.rb
@@ -0,0 +1,11 @@
+class CreateMicroposts < ActiveRecord::Migration
+  def change
+    create_table :microposts do |t|
+      t.string :year
+      t.string :content
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
